1dc540edb9614a5dec4aceaa89cdcc652da31a00
duberdico <duberdico@gmail.com>
2019-02-20 22:38:36 +0000

    added different modes

--- face_recon.py
+++ face_recon.py
@@ -3,6 +3,8 @@
 import imutils
 from imutils import paths
 from imutils import face_utils
+import matplotlib
+import matplotlib.pyplot as plt
 import numpy as np
 import argparse
 import imutils
@@ -11,6 +13,7 @@ import cv2
 import os
 import dlib
 import face_recognition
+from scipy.spatial import distance as dist
 
 def rect_to_bb(rect):
 	# take a bounding predicted by dlib and convert it
@@ -24,6 +27,22 @@ def rect_to_bb(rect):
 	# return a tuple of (x, y, w, h)
 	return (x, y, w, h)
 
+	
+def eye_aspect_ratio(eye):
+	# compute the euclidean distances between the two sets of
+	# vertical eye landmarks (x, y)-coordinates
+	A = dist.euclidean(eye[1], eye[5])
+	B = dist.euclidean(eye[2], eye[4])
+ 
+	# compute the euclidean distance between the horizontal
+	# eye landmark (x, y)-coordinates
+	C = dist.euclidean(eye[0], eye[3])
+ 
+	# compute the eye aspect ratio
+	ear = (A + B) / (2.0 * C)
+ 
+	# return the eye aspect ratio
+	return ear
 
 def shape_to_np(shape, dtype="int"):
 	# initialize the list of (x, y)-coordinates
@@ -38,24 +57,45 @@ def shape_to_np(shape, dtype="int"):
 	return coords
 
 
+ears = np.full((50,1),np.nan)
+
+# Initialize plot.
+fig, ax = plt.subplots()
+ax.set_xlabel('Time')
+ax.set_ylabel('Eyes')
+
+lw = 3
+alpha = 0.9
+line, = ax.plot(np.array(range(50)),ears, c='b', lw=lw)
+plt.xlim((0,50))
+plt.grid()
+plt.ion()
+plt.show()
+
+
 # initialize the video stream, then allow the camera sensor to warm up
 cap = cv2.VideoCapture(0)
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
 #the network has already been trained to create 128-d embeddings on a dataset of ~3 million images.
+mode = 0
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
 
+print(ears.shape)
+ear = np.full((1,1),np.nan)
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
-
+    frame = imutils.resize(frame, width=800)
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #gray = frame
 
     rects = detector(gray, 1)
 
     # Display the resulting frame
-    
     boxes = face_recognition.face_locations(gray, model="hog")
     
     # loop over the face detections
@@ -71,20 +111,48 @@ while(True):
         (x, y, w, h) = face_utils.rect_to_bb(rect)
         cv2.rectangle(gray, (x, y), (x + w, y + h), (0, 255, 0), 2)
     
-        # show the face number
-        cv2.putText(gray, "Face #{}".format(i + 1), (x - 10, y - 10),
-            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        if mode > 0:
+            # show the face number
+            cv2.putText(gray, "Face #{}".format(i + 1), (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
     
-        # loop over the (x, y)-coordinates for the facial landmarks
-        # and draw them on the image
-        for (x, y) in shape:
-            cv2.circle(gray, (x, y), 1, (0, 0, 255), -1)
+        if mode == 1:
+            # loop over the (x, y)-coordinates for the facial landmarks
+            # and draw them on the image
+            for (x, y) in shape:
+                cv2.circle(gray, (x, y), 4, (0, 0, 255), -1)
+        
+        if mode == 2:
+            leftEye = shape[lStart:lEnd]
+            rightEye = shape[rStart:rEnd]
+            leftEAR = eye_aspect_ratio(leftEye)
+            rightEAR = eye_aspect_ratio(rightEye)
+            #   average the eye aspect ratio together for both eyes
+            ear = (leftEAR + rightEAR) / 2.0
+            leftEyeHull = cv2.convexHull(leftEye)
+            rightEyeHull = cv2.convexHull(rightEye)
+            cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+            cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+            ears = np.append(ears[1:],ear)
+            line.set_ydata(ears)
+            plt.ylim((0,np.nanmax(ears)))
+            fig.canvas.draw()
+        else:
+            ears = np.full((50,1),np.nan)
+
+
 
     cv2.imshow('frame',gray)
     
     #encodings = face_recognition.face_encodings(gray, boxes)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    key = cv2.waitKey(1)
+    if key & 0xFF == ord('q'):
         break
+    if key & 0xFF == ord('a'):
+        mode = 0
+    if key & 0xFF == ord('s'):
+        mode = 1
+    if key & 0xFF == ord('d'):
+        mode = 2
 
 # When everything done, release the capture
 cap.release()
